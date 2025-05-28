0c14a9f4bc8ee6c9fc8ddab8699fe185c084e486
UminduH <73205708+UminduH@users.noreply.github.com>
2025-05-28 09:49:46 +0530

    Create about section with responsive design and include smooth navigation between sections

--- script.js
+++ script.js
@@ -18,3 +18,51 @@ function handleMenuVisibility() {
 }
 
 window.addEventListener('resize', handleMenuVisibility);
+
+
+// Smooth navigation of sections
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const targetId = anchor.getAttribute('href');
+    if (targetId === '#') return;
+
+    const tartgetElement = document.querySelector(targetId);
+    if (tartgetElement) {
+      window.scrollTo({
+        top: tartgetElement.offsetTop - 80,
+        behavior: 'smooth'
+      });
+
+      const mobileMenu = document.getElementById('js-mobile-nav-bar')
+      if (mobileMenu && !mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.add('hidden');
+      }
+    }
+  });
+});
+
+// Update active link in header
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+  let currentSection = '';
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (pageYOffset >= sectionTop - 100) {
+      currentSection = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove('active');
+
+    if (link.getAttribute('href') === `#${currentSection}`) {
+      link.classList.add('active');
+    }
+  });
+});
